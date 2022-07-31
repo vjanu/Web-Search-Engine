c0be27f4d8dc3992b7088b55d855ea7f5da46255
Dhruv Patel <80629263+DhruvPatel96@users.noreply.github.com>
2022-07-31 02:36:05 -0400

    Search V1.2

--- src/main/java/com/acc/se/features/WordSearch.java
+++ src/main/java/com/acc/se/features/WordSearch.java
@@ -4,17 +4,34 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class WordSearch {
 
     static String path = System.getProperty("user.dir");
+    static HashMap<String, Integer> userdata = new HashMap<String, Integer>();
 
-
-
-    public static String filename(int i) // function to return file name
+    public static void mapdata(String str, int i)
     {
-        File folder = new File("E:\\setext");
+        userdata.put(str,i);
+    }
+    public static void getdata()
+    {
+        System.out.println(userdata);
+    }
+
+    public static String txtfilename(int i) // function to return file name
+    {
+        File folder = new File("E:\\webpage\\text");
+        String[] path;
+        path = folder.list();
+        return path[i];
+    }
+
+    public static String htmlfilename(int i) // function to return file name
+    {
+        File folder = new File("E:\\webpage\\html");
         String[] path;
         path = folder.list();
         return path[i];
@@ -22,7 +39,7 @@ public class WordSearch {
 
     public static void wordSearch(String input) throws IOException // function to search for word in ALL text file
     {
-        File folder_ = new File("E:\\setext\\");
+        File folder_ = new File("E:\\webpage\\text\\");
         File[] file_list = folder_.listFiles();
         String[] words = null;  //Intialize the word Array
         String str;
@@ -31,7 +48,7 @@ public class WordSearch {
 
         for (int i = 0; i < file_list.length; i++) {
             if (file_list[i].isFile()) {
-                FileReader file_reader = new FileReader("E:\\setext\\" + file_list[i].getName());
+                FileReader file_reader = new FileReader("E:\\webpage\\text\\" + file_list[i].getName());
                 BufferedReader buff = new BufferedReader(file_reader); //Creation of BufferedReader object
 
                 while ((str = buff.readLine()) != null)   //Reading Content from the file
@@ -47,7 +64,8 @@ public class WordSearch {
                 if (count != 0)  //Check for count not equal to zero
                 {
                     System.out.println("The given word is present for " + count + " Times in the file");
-                    System.out.println(filename(i)+"\n");
+                    System.out.println(txtfilename(i)+"\n");
+                    mapdata(htmlfilename(i),count);
                 } else {
                     System.out.println("The given word is not present in the file");
                 }
@@ -65,6 +83,9 @@ public class WordSearch {
         System.out.println("Enter the Word to Search");
         String input = ipobj.nextLine();
         wordSearch(input);
+        getdata();
+
+
     }
 
 }
